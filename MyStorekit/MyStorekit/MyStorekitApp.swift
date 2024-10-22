5d8e3fd79aeafef4af1755a696d7e90abafd9f0c
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2024-10-22 15:56:54 +0900

    First commit

---
+++ MyStorekit/MyStorekit/MyStorekitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStorekitApp.swift
+//  MyStorekit
+//
+//  Created by Swift-Beginners on 2024/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyStorekitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyStorekit/README.md
@@ -0,0 +1,8 @@
+# Storekit2
+Storekit2サンプルコード
+
+Storekit2の仕組みとコードを理解するためのサンプルコード。
+
+参考にしたページ
+- [StoreKit 2によるiOSのアプリ内課金のチュートリアル](https://www.revenuecat.com/blog/engineering/ios-in-app-subscription-tutorial-with-storekit-2-and-swift-jp/)
+
